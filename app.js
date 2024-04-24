1af4d0893a274ada29399fab4fb5e9b2258d3403
camjuresich <camjuresich@gmail.com>
2024-04-23 20:35:10 -1000

    Added standard chess notation ids to spaces

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 // Create a loop that draws the board.
 // the board will be drawn of many different divs 
-// I believe css grid would be good to use in this case
+// I believe css grid would be good to use in this case 
+// https://dev.to/hira_zaira/create-a-chessboard-using-css-grid-3iil
+// 
 const drawBoard = function(){
     const board = document.getElementById("board")
-    
+    const aToH = "abcdefgh"
+    const oneToEight = "87654321"
     
     for (let i = 0; i < 8; i++) {
+        let row = oneToEight[i];
         let w = "white"
         let b = "black"
         if(i % 2 === 1) {
             w = "black"
             b = "white"
         }
-        for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            col = aToH[j];
             const space = document.createElement("div")
             board.appendChild(space)
-            if (i % 2 === 0) {
+            if (j % 2 === 0) {
                 space.classList.add(w)
             } else {
                 space.classList.add(b)
             }
+            space.id = `${col}${row}`
             space.classList.add("space")
         }
     }
