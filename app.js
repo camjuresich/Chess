67760fb96f992b798c637a88901636e9b58b866a
camjuresich <camjuresich@gmail.com>
2024-04-25 20:18:49 -1000

    Added onlick functions to all spaces

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const setUpGame = function() {
             const currentPosition = positions[i];
             const currentElement = document.createElement('img')
             currentElement.src = source
-            console.log(currentElement)
+            // console.log(currentElement)
             if (currentPosition.includes('1') || currentPosition.includes('2')) {
                 currentElement.classList.add('whitePiece')
             } else {
@@ -60,7 +60,8 @@ const setUpGame = function() {
             
         }
     }
-    const pawns = ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7', 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'];
+    const pawns = [
+        'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7', 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'];
     const rooks = ['a8', 'h8', 'a1', 'h1'];
     const knights = ['b8', 'g8', 'b1', 'g1'];
     const bishops = ['c8', 'f8', 'c1', 'f1'];
@@ -73,6 +74,52 @@ const setUpGame = function() {
     setPiece(kings, 'svgs/king-svgrepo-com.svg')
     setPiece(queens, 'svgs/queen-svgrepo-com.svg')
 }
+const movePiece = function () {
+    // if an svg is on a space allow that piece to be clicked.
+    // when piece is clicked allow piece to move anywhere there is not a piece
+    const aToH = "abcdefgh"
+    const oneToEight = "87654321"
+    for (let i = 0; i < aToH.length; i++) {
+        let letter = aToH[i];
+        for (let j = 0; j < oneToEight.length; j++) {
+            let number = oneToEight[j];
+            let currentSpace = `${letter}${number}`
+            let node = document.getElementById(currentSpace)
+            if (node.querySelector('img')) {
+                node.onclick = function() {
+                    console.log('space is occupied')
+                }
+            } else {
+                node.onclick = function() {
+                    console.log('space is not occupied')
+                }
+            }
+        } 
+    }
 
+    
+}
+/*
+Your king can not have moved- 
+Once your king moves, you can no longer castle, even if you move the king back to the starting square. 
+Many strategies involve forcing the opponent’s king to move just for this reason. 
+
+Your rook can not have moved- 
+If you move your rook, you can’t castle on that side anymore.
+ Both the king and the rook you are castling with can’t have moved. 
+
+Your king can NOT be in check- 
+Though castling often looks like an appealing escape, you can’t castle while you are in check!
+Once you are out of check, then you can castle. Unlike moving, being checked does not remove the ability to castle later. 
+
+Your king can not pass through check- 
+If any square the king moves over or moves onto would put you in check, 
+you can’t castle. You’ll have to get rid of that pesky attacking piece first!
+
+No pieces can be between the king and rook- 
+All the spaces between the king and rook must be empty. 
+This is part of why it’s so important to get your pieces out into the game as soon as possible! 
+*/
 drawBoard();
 setUpGame();
+movePiece();
